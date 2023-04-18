df12c366e9f6d7bef29cdb80db992d31a594329c
Siphesihle Shabalala <g21s8633@campus.ru.ac.za>
2023-04-18 14:01:32 +0200

    Modified App.java to print in json format

--- app/src/main/java/sdp/t2/App.java
+++ app/src/main/java/sdp/t2/App.java
@@ -6,6 +6,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class App {
@@ -18,29 +19,40 @@ public class App {
             DocumentBuilder builder = factory.newDocumentBuilder();
 
             // Parse the XML file
-            File xmlFile = new File("dataSample.xml");
+            File xmlFile = new File("data.xml");
             Document doc = builder.parse(xmlFile);
+
+            // Get the root element of the document
+            Element root = doc.getDocumentElement();
+
+            // Get a list of all child elements of the root element
+            NodeList nodeList = root.getChildNodes();
 
             // Get user input for fields to output
             Scanner scanner = new Scanner(System.in);
             System.out.print("Enter fields to output (comma-separated): ");
             String[] fields = scanner.nextLine().split(",");
 
-            // Get a list of all records
-            NodeList records = doc.getElementsByTagName("record");
-
-            // Loop through each record and print out the selected fields
-            for (int i = 0; i < records.getLength(); i++) {
-                Element record = (Element) records.item(i);
-                for (String field : fields) {
-                    NodeList fieldNodes = record.getElementsByTagName(field.trim());
-                    for (int j = 0; j < fieldNodes.getLength(); j++) {
-                        Element fieldElement = (Element) fieldNodes.item(j);
-                        System.out.println(field + ": " + fieldElement.getTextContent());
+            // Loop through each child element and print out the selected fields in JSON format
+            System.out.println("[");
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                if (nodeList.item(i).getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) nodeList.item(i);
+                    boolean firstField = true;
+                    for (String field : fields) {
+                        if (element.getTagName().equals(field.trim())) {
+                            if (!firstField) {
+                                System.out.print(", ");
+                            } else {
+                                firstField = false;
+                            }
+                            System.out.print("\"" + element.getTagName() + "\": \"" + element.getTextContent() + "\"");
+                        }
                     }
+                    System.out.println();
                 }
-                System.out.println();
             }
+            System.out.println("]");
         } catch (Exception e) {
             e.printStackTrace();
         }
